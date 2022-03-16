9ff1de22fec1d38bc1aed6ab05b42caf74cf364b
Tony Bowden <tony@tmtm.com>
2022-03-16 12:07:10 +0200

    compare enwiki 2nd Vice-Presidents

---
+++ sources/second-vice-president/scraper.rb
@@ -0,0 +1,28 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Picture'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[no img name start end].freeze
+    end
+
+    def empty?
+      super || noko.text.include?('vacant')
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv

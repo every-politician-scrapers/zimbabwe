7f1e90c9d9736d3669e659793d877bb9291c7f3a
Tony Bowden <tony@tmtm.com>
2022-03-16 15:10:46 +0200

    compare enwiki Bank Governor

---
+++ sources/bank-governor/scraper.rb
@@ -0,0 +1,45 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  # TODO: make this easier to override
+  def holder_entries
+    noko.xpath('.//h3[contains(.,"Reserve Bank of Zimbabwe")]/following::ul[1]//li[a]')
+  end
+
+  class Officeholder < OfficeholderBase
+    def raw_combo_date
+      noko.xpath('text()').text.gsub(/^\s*,/, '').gsub(/, .*/, '').tidy
+    end
+
+    def combo_date?
+      true
+    end
+
+    def item
+      name_cell.attr('wikidata')
+    end
+
+    def name
+      nake_cell.text.tidy
+    end
+
+    def name_cell
+      noko.css('a').first
+    end
+
+    def empty?
+      false
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv

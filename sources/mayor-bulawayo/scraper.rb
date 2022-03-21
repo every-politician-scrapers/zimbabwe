4a03e0398f29e9975f413d910720f31c087caa24
Tony Bowden <tony@tmtm.com>
2022-03-21 13:55:17 +0200

    compare enwiki Mayors of Bulawayo

---
+++ sources/mayor-bulawayo/scraper.rb
@@ -0,0 +1,28 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Mayor'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[name start end].freeze
+    end
+
+    def empty?
+      startDate[/(\d{4})/, 1].to_i < 2000
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv

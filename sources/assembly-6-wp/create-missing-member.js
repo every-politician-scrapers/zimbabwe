f8d08db98df062b3b0173117fec9311a1b3b13dc
Tony Bowden <tony@tmtm.com>
2022-03-21 08:20:52 +0200

    Assembly 6: compare to enwiki

---
+++ sources/assembly-6-wp/create-missing-member.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+let rawmeta = fs.readFileSync('meta.json');
+let meta = JSON.parse(rawmeta);
+
+module.exports = (label,party) => {
+  mem = {
+    value: meta.position,
+    qualifiers: {
+      P2937: meta.term.id,
+      P4100: party,
+    },
+    references: {
+      P4656: meta.source,
+      P813: new Date().toISOString().split('T')[0],
+      P1810: label,
+    }
+  }
+
+  claims = {
+    P31: { value: 'Q5' }, // human
+    P106: { value: 'Q82955' }, // politician
+    P39: mem,
+  }
+
+  return {
+    type: 'item',
+    labels: { en: label },
+    descriptions: { en: 'Zimbabwean politician' },
+    claims: claims,
+  }
+}

69d4d40217eab187d008aa5531d9a0bff1cf42bb
Tony Bowden <tony@tmtm.com>
2022-04-06 15:47:06 +0300

    improve diffs

--- sources/assembly-8-ep/diff.rb
+++ sources/assembly-8-ep/diff.rb
@@ -3,5 +3,11 @@
 
 require 'every_politician_scraper/comparison'
 
-diff = EveryPoliticianScraper::NulllessComparison.new('wikidata.csv', 'scraped.csv').diff
+class Comparison < EveryPoliticianScraper::NulllessComparison
+  def columns
+    super - %i[partylabel startdate enddate]
+  end
+end
+
+diff = Comparison.new('wikidata.csv', 'scraped.csv').diff
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)

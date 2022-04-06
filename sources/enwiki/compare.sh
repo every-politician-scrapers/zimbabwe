f64695c3b44ff270108d5e40a42bc19177699709
Tony Bowden <tony@tmtm.com>
2022-04-06 11:34:04 +0300

    make comparisons less verbose

--- sources/enwiki/compare.sh
+++ sources/enwiki/compare.sh
@@ -2,8 +2,8 @@
 
 cd sources/enwiki/
 
-bundle exec ruby scraper.rb | ifne tee scraped.csv
-wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' | qsv dedup -s psid | ifne tee wikidata.csv
+bundle exec ruby scraper.rb > scraped.csv
+wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' | qsv dedup -s psid > wikidata.csv
 bundle exec ruby diff.rb | tee diff.csv
 
 cd -

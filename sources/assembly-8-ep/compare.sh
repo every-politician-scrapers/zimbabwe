7473db427e82d7e081a5cf8ef221cc964fcb996d
Tony Bowden <tony@tmtm.com>
2022-04-06 15:53:06 +0300

    silence cd -

--- sources/assembly-8-ep/compare.sh
+++ sources/assembly-8-ep/compare.sh
@@ -7,4 +7,4 @@ cd $(dirname $0)
 wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | qsv dedup -s psid | qsv sort -s name,startDate > wikidata.csv
 bundle exec ruby diff.rb | qsv sort -s name | tee diff.csv
 
-cd -
+cd ~-
--- sources/enwiki/compare.sh
+++ sources/enwiki/compare.sh
@@ -6,4 +6,4 @@ bundle exec ruby scraper.rb > scraped.csv
 wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' | qsv dedup -s psid > wikidata.csv
 bundle exec ruby diff.rb | tee diff.csv
 
-cd -
+cd ~-

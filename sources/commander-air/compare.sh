609daed8a097782110d48cba3bf08c0f0c1a6f10
Tony Bowden <tony@tmtm.com>
2022-06-17 13:46:45 +0300

    update to latest wpoffice

--- sources/commander-air/compare.sh
+++ sources/commander-air/compare.sh
@@ -2,8 +2,8 @@
 
 cd $(dirname $0)
 
-bundle exec ruby scraper.rb $(jq -r .source meta.json) > scraped.csv
-wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | qsv dedup -s psid > wikidata.csv
-bundle exec ruby diff.rb | qsv sort -s startdate | qsv select 1,3,2,4,5 | tee diff.csv
+curl -LsS https://github.com/every-politician-scrapers/shared/raw/main/wpoffice.tgz | tar xfz - --strip-components=1
+bash run-comparison.sh
+git clean -qfX
 
 cd ~-
